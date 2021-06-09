d898b54baf7b6f6562091b7fb9bc691d1f3fa346
Sherwin Ocubillo <sherwin.ocubillo@bapplware.com>
2021-06-09 08:16:37 +0800

    configuring class models relationship

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+	protected $fillable = ['room_id','user_id','from','to'];
+
+	public function room()
+	{
+		return $this->belongsTo(Room::class);
+	}
 }
