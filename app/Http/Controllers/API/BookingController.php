1490eb9e45e013d54d769c361df478202ee62b0e
Sherwin Ocubillo <sherwin.ocubillo@bapplware.com>
2021-06-09 08:23:32 +0800

    creating crud controller methods

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -2,10 +2,47 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
 
 class BookingController extends Controller
 {
-    //
+	public function index()
+	{
+		$books = Booking::all()->toArray();
+		return array_reverse($books);
+	}
+
+	public function add(Request $request)
+	{
+		$book = new Booking([
+			'name' => $request->name
+		]);
+		$book->save();
+
+		return response()->json('Booking successfully added');
+	}
+
+	public function edit($id)
+	{
+		$book = Booking::find($id);
+		return response()->json($book);
+	}
+
+	public function update($id, Request $request)
+	{
+		$book = Booking::find($id);
+		$book->update($request->all());
+
+		return response()->json('Booking successfully updated');
+	}
+
+	public function delete($id)
+	{
+		$book = Booking::find($id);
+		$book->delete();
+
+		return response()->json('Booking successfully deleted');
+	}
 }
--- app/Http/Controllers/API/RoomController.php
+++ app/Http/Controllers/API/RoomController.php
@@ -2,10 +2,47 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Room;
 
 class RoomController extends Controller
 {
-    //
+	public function index()
+	{
+		$books = Room::all()->toArray();
+		return array_reverse($books);
+	}
+
+	public function add(Request $request)
+	{
+		$book = new Room([
+			'name' => $request->name
+		]);
+		$book->save();
+
+		return response()->json('The room successfully added');
+	}
+
+	public function edit($id)
+	{
+		$book = Room::find($id);
+		return response()->json($book);
+	}
+
+	public function update($id, Request $request)
+	{
+		$book = Room::find($id);
+		$book->update($request->all());
+
+		return response()->json('The room successfully updated');
+	}
+
+	public function delete($id)
+	{
+		$book = Room::find($id);
+		$book->delete();
+
+		return response()->json('The room successfully deleted');
+	}
 }
